a161f17a024f7fdd8a09508bbbbb88304f849762
cszsm <zscseh93@gmail.com>
2018-10-14 17:28:32 +0200

    array_to_audio

--- preprocessing/audio_import.py
+++ preprocessing/audio_import.py
@@ -26,3 +26,8 @@ def get_files_from_library(path, ext='wav'):
 def audio_to_array(file, ext='wav'):
     audio = AudioSegment.from_file(file, format=ext).set_channels(1)
     return np.array(audio.get_array_of_samples()), audio.frame_rate, audio.duration_seconds
+
+
+def array_to_audio(array, path, ext='mp3'):
+    audio = AudioSegment(array, sample_width=2, frame_rate=44100, channels=1)
+    audio.export(path + '.' + ext, format(ext))
