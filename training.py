f95d645e469fc7616d314776bc40586c11e0967f
m2kohand <ka1219@hszk.bme.hu>
2018-10-14 20:07:09 +0200

    training funct

--- training.py
+++ training.py
@@ -1,7 +1,6 @@
-import numpy as np
-
 import os.path
 
+import numpy as np
 from keras.callbacks import ModelCheckpoint
 
 from model.autoencoder import conv_ae_1d
@@ -24,28 +23,29 @@ decoder_setup = [
     (64, (5), 4)
 ]
 
-# model = conv_ae_1d(input_shape=(16384, 1), encoder_setup=encoder_setup, decoder_setup=decoder_setup)
-# print(model.summary())
-#
-# b = audio_segment_generator(16384, 2, 'train', 'mp3')
-# d = next(b)
-# print(d[0].shape)
+model = conv_ae_1d(input_shape=(16384, 1), encoder_setup=encoder_setup, decoder_setup=decoder_setup)
+print(model.summary())
 
-# model.compile(optimizer='adam', loss='mse')
+b = audio_segment_generator(16384, 256, 'train', 'mp3')
+d = next(b)
+print(d[0].shape)
 
-# checkpoint_path = os.path.join('checkpoints', 'epoch-{epoch:02d}-{loss:.4f}.hdf5')
-# checkpoint = ModelCheckpoint(checkpoint_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+model.compile(optimizer='sgd', loss='mse')
 
-# model.fit_generator(b, epochs=2, steps_per_epoch=2, callbacks=[checkpoint])
+checkpoint_path = os.path.join('checkpoints', 'epoch-{epoch:02d}-{loss:.4f}.hdf5')
+checkpoint = ModelCheckpoint(checkpoint_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+
+model.fit_generator(b, epochs=2, steps_per_epoch=100, callbacks=[checkpoint])
 
 validation_samples = audio_segments_from_single_file(16384, 'nocturne.mp3')
 
-preds = np.array([])
+preds = np.array([], dtype=np.int16)
 
 for s in validation_samples:
-    # pred = model.predict(np.expand_dims(np.expand_dims(s, -1), 0))
-
-    preds = np.append(preds, (s * 16384).astype(np.int32))
-
+    pred = model.predict(np.expand_dims(np.expand_dims(s, -1), 0))
+    preds = np.append(preds, (pred * 16384).astype(np.int16))
 
 array_to_audio(preds, 'mix')
+# lstm_model = conv_lstm_ae_1d(timesteps=10, input_shape=(16384, 1), encoder_setup=encoder_setup, lstm_num=2,
+#                              decoder_setup=decoder_setup)
+# print(lstm_model.summary())
